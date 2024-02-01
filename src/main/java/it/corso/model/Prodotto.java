2da0971eafe7753617a5e179206f30f5f75e060f
ChiaraFar18 <chiarettafarese@gmail.com>
2024-02-01 12:51:04 +0100

    Aggiornamento Utente, espressioni regolari

--- src/main/java/it/corso/model/Prodotto.java
+++ src/main/java/it/corso/model/Prodotto.java
@@ -1,9 +1,7 @@
 package it.corso.model;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- src/main/java/it/corso/model/Utente.java
+++ src/main/java/it/corso/model/Utente.java
@@ -8,47 +8,58 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name="utente")
 public class Utente {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private int id;
 	
+	@Pattern(regexp = "[a-zA-Z'.\\s]{2,50}", message= "Caratteri non ammessi nel campo nome!")
 	@Column(name="nome")
 	private String nome;
 	
+	@Pattern(regexp = "[a-zA-Z'.\\s]{2,50}", message= "Caratteri non ammessi nel campo nome!")
 	@Column(name="cognome")
 	private String cognome;
 	
+	@Pattern(regexp = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$", message="Formato data non corretto!")
 	@Column(name="dataDiNascita")
 	private LocalDate dataDiNascita;
 	
+	@Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,30}$", message="Formato indirizzo email non corretto!")
 	@Column(name="email")
 	private String email;
 	
+	@Pattern(regexp = "^[0-9]{9,15}$", message="Numero di telefono non corretto!")
 	@Column(name="numeroDiTelefono")
 	private String numeroDiTelefono;
 	
+	@Pattern(regexp = "[a-zA-Z'.\\s]{2,50}", message= "Caratteri non ammessi nel campo città!")
 	@Column(name="citta")
 	private String citta;
 	
+	@Pattern(regexp = "[a-zA-Z'.\\s]{5,100}", message= "Caratteri non ammessi nel campo indirizzo!")
 	@Column(name="indirizzo")
 	private String indirizzo;
 	
+	@Pattern(regexp = "[A-Z]{2}", message= "Caratteri non ammessi nel campo provincia!")
 	@Column(name="provincia")
 	private String provincia;
 	
+	@Pattern(regexp = "[0-9]{5}$", message= "Caratteri non ammessi nel campo provincia!")
 	@Column(name="cap")
 	private String cap;
 	
+	@Pattern(regexp = "[a-zA-Z0-9._-]{2,15}", message= "Caratteri non ammessi nel campo username!")
 	@Column(name="nomeUtente")
 	private String nomeUtente;
 	
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]{8,}$", message= "La password non rispetta le seguenti caratteristiche: deve avere almeno 8 caratteri, almeno una lettera maiuscola, una lettera minuscola, un numero e un carattere speciale tra @$!%*?&.")
 	@Column(name="password")
-	private String password;
-	
+	private String password;	
 	
 	
 	
