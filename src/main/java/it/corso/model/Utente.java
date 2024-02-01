68264bff912b59ddc0dbac9d8e211c5fcb8a8783
MafaldaDamiani <mafalda.damiani@gmail.com>
2024-02-01 11:28:26 +0100

    Update utente.java

--- src/main/java/it/corso/model/Utente.java
+++ src/main/java/it/corso/model/Utente.java
@@ -1,5 +1,128 @@
 package it.corso.model;
 
-public class Utente {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="utente")
+public class Utente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="cognome")
+	private String cognome;
+	
+	@Column(name="dataDiNascita")
+	private LocalDate dataDiNascita;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="numeroDiTelefono")
+	private String numeroDiTelefono;
+	
+	@Column(name="citta")
+	private String citta;
+	
+	@Column(name="indirizzo")
+	private String indirizzo;
+	
+	@Column(name="provincia")
+	private String provincia;
+	
+	@Column(name="cap")
+	private String cap;
+	
+	@Column(name="nomeUtente")
+	private String nomeUtente;
+	
+	@Column(name="password")
+	private String password;
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public LocalDate getDataDiNascita() {
+		return dataDiNascita;
+	}
+	public void setDataDiNascita(LocalDate dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNumeroDiTelefono() {
+		return numeroDiTelefono;
+	}
+	public void setNumeroDiTelefono(String numeroDiTelefono) {
+		this.numeroDiTelefono = numeroDiTelefono;
+	}
+	public String getCitta() {
+		return citta;
+	}
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getCap() {
+		return cap;
+	}
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+	public String getNomeUtente() {
+		return nomeUtente;
+	}
+	public void setNomeUtente(String nomeUtente) {
+		this.nomeUtente = nomeUtente;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
