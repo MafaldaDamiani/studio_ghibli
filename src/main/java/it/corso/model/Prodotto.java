e4d1e32e887157c054c28b4e0c592bc87599d709
ChiaraFar18 <chiarettafarese@gmail.com>
2024-02-01 11:39:48 +0100

    Update prodotto.java

--- src/main/java/it/corso/model/Prodotto.java
+++ src/main/java/it/corso/model/Prodotto.java
@@ -2,11 +2,14 @@ package it.corso.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -39,6 +42,10 @@ public class Prodotto {
 	
 	@Column(name="quantita_disponibile")
 	private int quantitaDisponibile;
+	
+	@ManyToOne(cascade = CascadeType.REFRESH)
+	@JoinColumn(name="fk_id_film",referencedColumnName = "id" )
+	private Film film; 
 	
 	public int getId() {
 		return id;
