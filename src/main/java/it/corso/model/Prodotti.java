1a6e92dc89dec0a7d06d5d367f987d06da0ddfdf
ChiaraFar18 <chiarettafarese@gmail.com>
2024-02-01 11:19:28 +0100

    prodotti.java

--- src/main/java/it/corso/model/Prodotti.java
+++ src/main/java/it/corso/model/Prodotti.java
@@ -1,6 +1,98 @@
 package it.corso.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="prodotti")
 public class Prodotti {
-	private int id
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="tipologia_prodotto")
+	private String tipologiaProdotto;
+	
+	@Column(name="categoria")
+	private String categoria; 
+	
+	@Column(name="descrizione")
+	private String descrizione;
+	
+	@Column(name="prezzo")
+	private double prezzo; 
+	
+	@Column(name="immagine")
+	private String immagine; 
+	
+	@Column(name="data_inserimento")
+	private LocalDate dataInserimento;
+	
+	@Column(name="quantita_disponibile")
+	private int quantitaDisponibile;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipologiaProdotto() {
+		return tipologiaProdotto;
+	}
+	public void setTipologiaProdotto(String tipologiaProdotto) {
+		this.tipologiaProdotto = tipologiaProdotto;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	public String getImmagine() {
+		return immagine;
+	}
+	public void setImmagine(String immagine) {
+		this.immagine = immagine;
+	}
+	public LocalDate getDataInserimento() {
+		return dataInserimento;
+	}
+	public void setDataInserimento(LocalDate dataInserimento) {
+		this.dataInserimento = dataInserimento;
+	}
+	public int getQuantitaDisponibile() {
+		return quantitaDisponibile;
+	}
+	public void setQuantitaDisponibile(int quantitaDisponibile) {
+		this.quantitaDisponibile = quantitaDisponibile;
+	} 
 
 }
