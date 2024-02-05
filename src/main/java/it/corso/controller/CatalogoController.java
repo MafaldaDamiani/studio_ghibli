48b4ad37e4289dbc6bfd96b790b47a2a32caef54
soletifra <francescasoleti42@gmail.com>
2024-02-05 09:31:26 +0100

    catalogo, css folder, CatalogoController

---
+++ src/main/java/it/corso/controller/CatalogoController.java
@@ -0,0 +1,64 @@
+package it.corso.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import it.corso.model.Prodotto;
+import it.corso.service.ProdottoService;
+
+// localhost:8080/catalogo
+@Controller
+@RequestMapping("/catalogo")
+public class CatalogoController {
+	@Autowired
+	private ProdottoService prodottoService;
+	@GetMapping 
+	public String getPaginaFilm(
+		@RequestParam("tipologia")String tipologia,
+		@RequestParam(name = "filtro", required = false)String filtro,
+		@RequestParam(name = "nome", required = false)String tipoFiltro,
+		Model model) {
+		model.addAttribute("tipologiaLink", tipologia);
+		model.addAttribute("tipologiaTesto", tipologia.toUpperCase());
+		List <Prodotto> catalogo = null;
+		if (filtro != null) {
+			if(filtro.equals("Categoria")) {
+				catalogo = prodottoService.getProdottiPerCategoria(tipoFiltro);
+			}
+			else if (filtro.equals("Film")) {
+				int idFilm = 0;
+				try {
+					idFilm = Integer.parseInt(tipoFiltro);
+				} catch (Exception e) {
+					return "redirect:/categoria?tipologia=Merchandising";
+				}
+				catalogo = prodottoService.getMerchandisingPerFilm(idFilm);
+			}
+			else if(filtro.equals("Ordina")) {
+				if (tipoFiltro.equals("Crescente")) {
+					catalogo = prodottoService.getProdottiPerPrezzoCrescente(tipologia);
+				}
+				else {
+					catalogo = prodottoService.getProdottiPerPrezzoDecrescente(tipologia);
+				}
+			}
+			else if(filtro.equals("Genere")) {
+				catalogo = prodottoService.getFilmPerGenere(tipoFiltro);
+			}			
+			else if(filtro.equals("Rating")) {
+				catalogo = prodottoService.getFilmPerRating(tipoFiltro);
+			}
+			else if(filtro.equals("Noleggio")) {
+				catalogo = prodottoService.getFilmPerNoleggio(tipoFiltro);
+			}
+		}
+		else {
+			catalogo = prodottoService.getProdottiPerTipologia(tipologia);
+		}
+		model.addAttribute("catalogo", catalogo);
+		return "catalogo";
+	}
+}
